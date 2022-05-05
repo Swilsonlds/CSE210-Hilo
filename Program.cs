1956875d4e821d48ed008475fa8d007ef0572f88
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2022-05-05 10:44:48 -0600

    initializing

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSE210_Hilo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
